728ee25b08004beeedec29eeb43334d4c9f171e9
Sam Tamburri <tamburrisam@gmail.com>
2020-04-14 21:00:26 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,7 +1,4 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
-
   def initialize(title)
     @title = title
   end
@@ -10,8 +7,32 @@ class Book
     @title
   end
 
-def turn_page
-  puts "Flipping the page... wow, you read fast!"
-end
+  def author=(author)
+    @author = author
+  end
+
+  def author
+    @author
+  end
+
+  def page_count=(num)
+    @page_count = num
+  end
+
+  def page_count
+    @page_count
+  end
+
+  def genre=(genre)
+    @genre = genre
+  end
+
+  def genre
+    @genre
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
 
 end
